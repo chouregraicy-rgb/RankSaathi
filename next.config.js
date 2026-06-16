83ba3c338bb35b2aa6618dd0d3ecc1f73f423311
chouregraicy-rgb <choure.graicy@gmail.com>
2026-06-17 00:28:34 +0530

    fix: add upload.wikimedia.org to next.config image domains

--- next.config.js
+++ next.config.js
@@ -24,6 +24,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
     ],
   },
 };
